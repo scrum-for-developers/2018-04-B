076658140618198fd2ad0685e4d3d138554bad15
Vincent Wagner <vincent.wagner@codecentric.de>
2018-04-13 16:02:15 +0200

    improved ui. label-Felder ausgerichtet, Menue auf jeder Seite, Tabellenfarben ,Version auf 1.5

--- worblehat-acceptancetests/src/main/java/de/codecentric/worblehat/acceptancetests/adapter/wrapper/PageElement.java
+++ worblehat-acceptancetests/src/main/java/de/codecentric/worblehat/acceptancetests/adapter/wrapper/PageElement.java
@@ -2,7 +2,7 @@ package de.codecentric.worblehat.acceptancetests.adapter.wrapper;
 
 public enum PageElement {
     ADDBOOKBUTTON("addBook"), BOOKLIST("bookList"), BORROWBOOKBUTTON("borrowBook"), ISBNERROR("isbn-error"),
-    RETURNALLBOOKSBUTTON("returnAllBooks"),RETURNBOOKBUTTON("returnBook"), SHOWMYBOOKSBUTTON("showMyBooks"), ERROR("error");
+    RETURNALLBOOKSBUTTON("returnAllBooks"),RETURNBOOKBUTTON("returnBook"), SHOWMYBOOKSBUTTON("showMyBooks"), ERROR("form-error");
 
 
     private String elementId;
